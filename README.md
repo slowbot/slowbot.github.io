b3bb2a4f137063be503427f14a83592071961acf
slowbot <jesse@slowbot.com>
2015-02-23 14:04:19 -0800

    Adding Middleman

---
+++ README.md
@@ -0,0 +1,22 @@
+mm-blank
+==================
+
+## Mister Machine Blank
+
+A starter project for front-end.
+
+Built with Middleman and Foundation.
+
+## Getting Started
+
+Run Bundle Install to Ruby Gem dependancies:
+
+`$ bundle install` 
+
+Run Bower Update to update JavaScript dependancies:
+
+`$ bower update`
+
+To start your Middleman Server run:
+
+`$ middleman server`
---
+++ source/javascripts/all.js
@@ -0,0 +1,20 @@
+//= require jquery/dist/jquery
+//= require foundation/js/foundation/foundation
+//= require foundation/js/foundation/foundation.offcanvas
+//= require foundation/js/foundation/foundation.reveal
+//= require foundation/js/foundation/foundation.dropdown
+//= require foundation/js/foundation/foundation.alert
+//= require foundation/js/foundation/foundation.abide
+//= require foundation/js/foundation/foundation.equalizer
+//= require google-code-prettify/bin/prettify.min.js
+//= require responsive-nav.js
+//= require wow.js
+/* require TweenMax.min.js */
+//= require SnapPlugin.js
+//= require snap.svg.js
+//= require jquery.scrollmagic.js
+//= require jquery-scrolltofixed.js
+//= require angular/angular
+//= require app
+/* require demo */
+//= require controllers/loan-calculator-controller
